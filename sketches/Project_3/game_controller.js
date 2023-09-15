c7dbdf521bf9387ebe294cb40bd8f9f2f62cea67
Hayden Gray <hayden.gray104@gmail.com>
2023-09-14 23:05:12 -0400

    reset game code

--- sketches/Project_3/game_controller.js
+++ sketches/Project_3/game_controller.js
@@ -12,16 +12,16 @@ class GameController {
     this.world.register_system(new Collision())
     this.world.register_system(new Movement())
 
-    // for (let i = 0; i < 3000; i++) {
-    //   this.world.spawn_entity(
-    //     [
-    //       new Enemy(),
-    //       new Sprite(this.sprite_manager.get_sprite('player')),
-    //       new Transform(createVector(Math.floor((i * 20) % 400), Math.floor(i % 400), 0)),
-    //       new Collider(10)
-    //     ]
-    //   )
-    // }
+    for (let i = 0; i < 3000; i++) {
+      this.world.spawn_entity(
+        [
+          new Enemy(),
+          new Sprite(this.sprite_manager.get_sprite('player')),
+          new Transform(createVector(Math.floor((i * 20) % 400), Math.floor(i % 400), 0)),
+          new Collider(10)
+        ]
+      )
+    }
 
     this.world.spawn_entity(
       [
@@ -52,13 +52,18 @@ class GameController {
       entity_count.html(`Entity Count: ${this.world.registry.entity_count}`)
       this.frametimes.length = 0
 
-      this.world.spawn_entity(
+      let id = this.world.spawn_entity(
         [
           new Enemy(),
           new Sprite(this.sprite_manager.get_sprite('player')),
           new Transform(createVector(200, 200)),
           new Collider(10)
         ]
+      )
+
+      this.world.remove_components(
+        id,
+        [ new Collider() ]
       )
     }
     this.frametimes.push(Math.round((1/deltaTime) * 1000))
